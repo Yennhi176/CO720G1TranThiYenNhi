262ae8f19bd5245b0904ac79efa50400853a4d59
Yennhi176 <nhitinh2206@gmail.com>
2020-09-10 08:37:02 +0700

    nop_bai

---
+++ module2/src/_02_vong_lap_trong_java/bai_tap/HienThiCacLoaiHinh.java
@@ -0,0 +1,25 @@
+package _02_vong_lap_trong_java.bai_tap;
+import java.util.Scanner;
+public class HienThiCacLoaiHinh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1.Print the rectangle");
+        System.out.println("2.Print the square triangle");
+        System.out.println("3.Print isosceles triangle");
+        System.out.println("4.Exit");
+        System.out.println("Enter your choice: ");
+        while (true){
+            int choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Print the rectangle");
+
+
+
+
+            }
+        }
+    }
+
+}
